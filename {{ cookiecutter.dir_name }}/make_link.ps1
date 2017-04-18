6218bf9c410a79e69f76f53a4e068abfe771ac37
Russell Keith-Magee <russell@keith-magee.com>
2017-04-18 08:17:16 +0800

    Corrected localization of script.

--- {{ cookiecutter.dir_name }}/make_link.ps1
+++ {{ cookiecutter.dir_name }}/make_link.ps1
@@ -1,9 +1,9 @@
 $WshShell = New-Object -ComObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("{{ }}\{{ cookiecutter.app_name }}\windows\{{ cookiecutter.formal_name }}.lnk")
-$Shortcut.TargetPath = "{{ }}\{{ cookiecutter.app_name }}\windows\python\pythonw.exe"
-$Shortcut.RelativePath = "{{ }}\{{ cookiecutter.app_name }}\windows\python\pythonw.exe"
+$Shortcut = $WshShell.CreateShortcut($PS1 + "\{{ cookiecutter.app_name }}\windows\{{ cookiecutter.formal_name }}.lnk")
+$Shortcut.TargetPath = $PS1 + "\{{ cookiecutter.app_name }}\windows\python\pythonw.exe"
+$Shortcut.RelativePath = $PS1 + "\{{ cookiecutter.app_name }}\windows\python\pythonw.exe"
 $Shortcut.Arguments = "app\start.py"
-$Shortcut.IconLocation = "{{ }}\{{ cookiecutter.app_name }}\windows\app\app.ico,0"
+$Shortcut.IconLocation = $PS1 + "\{{ cookiecutter.app_name }}\windows\app\app.ico,0"
 $Shortcut.Description = "{{ cookiecutter.description }}"
-$Shortcut.WorkingDirectory = "{{ }}\{{ cookiecutter.app_name }}\windows"
+$Shortcut.WorkingDirectory = $PS1 + "\{{ cookiecutter.app_name }}\windows"
 $Shortcut.Save()
