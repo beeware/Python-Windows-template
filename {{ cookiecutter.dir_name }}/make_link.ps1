495ccc66295799f4eb59350f3a58be2747f9fd89
Russell Keith-Magee <russell@keith-magee.com>
2017-04-18 08:36:48 +0800

    tweaked relocatability of make_link script.

--- {{ cookiecutter.dir_name }}/make_link.ps1
+++ {{ cookiecutter.dir_name }}/make_link.ps1
@@ -1,9 +1,7 @@
 $WshShell = New-Object -ComObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut($PS1 + "\{{ cookiecutter.app_name }}\windows\{{ cookiecutter.formal_name }}.lnk")
-$Shortcut.TargetPath = $PS1 + "\{{ cookiecutter.app_name }}\windows\python\pythonw.exe"
-$Shortcut.RelativePath = $PS1 + "\{{ cookiecutter.app_name }}\windows\python\pythonw.exe"
+$Shortcut = $WshShell.CreateShortcut($PSScriptRoot + "{{ cookiecutter.formal_name }}.lnk")
+$Shortcut.TargetPath = $PSScriptRoot + "python\pythonw.exe"
 $Shortcut.Arguments = "app\start.py"
-$Shortcut.IconLocation = $PS1 + "\{{ cookiecutter.app_name }}\windows\app\app.ico,0"
+$Shortcut.IconLocation = $PSScriptRoot + "app\app.ico,0"
 $Shortcut.Description = "{{ cookiecutter.description }}"
-$Shortcut.WorkingDirectory = $PS1 + "\{{ cookiecutter.app_name }}\windows"
 $Shortcut.Save()
