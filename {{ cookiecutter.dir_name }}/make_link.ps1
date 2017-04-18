a6603c0faec3ae69287f835d8de3b4c82b20b053
Russell Keith-Magee <russell@keith-magee.com>
2017-04-18 09:06:04 +0800

    Modified name of icon.

--- {{ cookiecutter.dir_name }}/make_link.ps1
+++ {{ cookiecutter.dir_name }}/make_link.ps1
@@ -2,6 +2,6 @@ $WshShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("{{ cookiecutter.formal_name }}.lnk")
 $Shortcut.TargetPath = $PSScriptRoot + "\python\pythonw.exe"
 $Shortcut.Arguments = "app\start.py"
-$Shortcut.IconLocation = $PSScriptRoot + "\app\app.ico,0"
+$Shortcut.IconLocation = $PSScriptRoot + "\app\{{ cookiecutter.app_name }}.ico,0"
 $Shortcut.Description = "{{ cookiecutter.description }}"
 $Shortcut.Save()
