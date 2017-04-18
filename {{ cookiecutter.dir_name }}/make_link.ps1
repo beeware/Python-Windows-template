4cd59490157dbc76451d8ba51b5b76c3c34b2ce3
Russell Keith-Magee <russell@keith-magee.com>
2017-04-18 08:39:32 +0800

    Added separators.

--- {{ cookiecutter.dir_name }}/make_link.ps1
+++ {{ cookiecutter.dir_name }}/make_link.ps1
@@ -1,7 +1,7 @@
 $WshShell = New-Object -ComObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut($PSScriptRoot + "{{ cookiecutter.formal_name }}.lnk")
-$Shortcut.TargetPath = $PSScriptRoot + "python\pythonw.exe"
+$Shortcut = $WshShell.CreateShortcut("{{ cookiecutter.formal_name }}.lnk")
+$Shortcut.TargetPath = $PSScriptRoot + "\python\pythonw.exe"
 $Shortcut.Arguments = "app\start.py"
-$Shortcut.IconLocation = $PSScriptRoot + "app\app.ico,0"
+$Shortcut.IconLocation = $PSScriptRoot + "\app\app.ico,0"
 $Shortcut.Description = "{{ cookiecutter.description }}"
 $Shortcut.Save()
