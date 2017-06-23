2341d3ea5f343e1e75aa3fa04611dd98d87e1c07
Russell Keith-Magee <russell@keith-magee.com>
2017-06-23 14:15:43 +0800

    Updated template to use WIX for windows installer.

--- {{ cookiecutter.dir_name }}/make_link.ps1
+++
@@ -1,7 +0,0 @@
-$WshShell = New-Object -ComObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("{{ cookiecutter.formal_name }}.lnk")
-$Shortcut.TargetPath = $PSScriptRoot + "\python\pythonw.exe"
-$Shortcut.Arguments = "app\start.py"
-$Shortcut.IconLocation = $PSScriptRoot + "\app\{{ cookiecutter.app_name }}.ico,0"
-$Shortcut.Description = "{{ cookiecutter.description }}"
-$Shortcut.Save()
